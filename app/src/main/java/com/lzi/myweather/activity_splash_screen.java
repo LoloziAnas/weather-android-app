9c8bad21336d55c36511e07e38befa9684b3ec30
LoloziAnas <lolozianas1@gmail.com>
2020-07-29 05:05:57 +0100

    update splash screen

--- app/src/main/java/com/lzi/myweather/activity_splash_screen.java
+++ app/src/main/java/com/lzi/myweather/activity_splash_screen.java
@@ -5,9 +5,16 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class activity_splash_screen extends AppCompatActivity {
 
+    Animation top_anim, bottom_anim;
+    private ImageView img_logo;
+    private TextView tv_logo;
     Handler handler;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,6 +22,18 @@ public class activity_splash_screen extends AppCompatActivity {
         //getWindow().setFlags(getWindowManager().LayoutParams.FLAG_FULLSCREEN,getWindowManager().LayoutParams.FLAG_FULLSCREEN );
         setContentView(R.layout.activity_splash_screen);
 
+        img_logo = findViewById(R.id.logo);
+        tv_logo = findViewById(R.id.tv_logo);
+
+        // Animation initialized
+        top_anim = AnimationUtils.loadAnimation(this,R.anim.top_animation);
+        bottom_anim = AnimationUtils.loadAnimation(this,R.anim.buttom_animation);
+
+        // Set animation
+        img_logo.setAnimation(top_anim);
+        tv_logo.setAnimation(bottom_anim);
+
+        //handler
         handler = new Handler();
         handler.postDelayed(new Runnable() {
             @Override
@@ -24,6 +43,10 @@ public class activity_splash_screen extends AppCompatActivity {
                 finish();
             }
         },5000);
+
+    }
+
+    private void animate(){
 
     }
 }
